40449ec9947b65f0c7b8cbae774dc622dcd0c83b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-09 17:46:23 +0100

    minor couch_db refactoring, add exe/legacy/sync_events.rbget_seminar_docs_by_month

--- lib/wp_event/couch_import/couch_db.rb
+++ lib/wp_event/couch_import/couch_db.rb
@@ -4,9 +4,20 @@ require 'rest-client'
 module WPEvent
   module CouchImport
     module CouchDB
+      BASE_URL = "http://localhost:5984/fk_seminar/"
+
       def self.get_doc uuid
-        base_url = "http://localhost:5984/fk_seminar/"
-        JSON.parse RestClient.get(base_url + uuid)
+        JSON.parse RestClient.get(BASE_URL + uuid)
+      end
+
+      # While this might be abstracted, it might as well be
+      # considered smelly legacy shortcut.
+      def self.get_seminar_docs_by_month year, month
+        url = BASE_URL + "_design/sl_seminar/_view/seminar_by_month"
+        url = url + "?key=#{URI::encode([year.to_s, "%.2d"%month.to_i].to_json)}"
+        # Fetch the docs, one by one.
+        rows = JSON.parse(RestClient.get(url))["rows"]
+        rows.map{|r| get_doc r["id"]}
       end
     end
   end
