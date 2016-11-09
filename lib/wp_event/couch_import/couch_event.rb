6f948b08691b8cc1e6140baa9cfc12658fd674d4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-09 20:32:49 +0100

    extract CouchEvent#from_couch_document

--- lib/wp_event/couch_import/couch_event.rb
+++ lib/wp_event/couch_import/couch_event.rb
@@ -14,18 +14,36 @@ module WPEvent
         @document    = document
       end
 
+      def self.from_couch_doc document
+        WPEvent::CouchImport::CouchEvent.new document["_id"],
+          document.dig("g_value", "title"),
+          document.dig("g_value", "description_long"),
+          Date.strptime(document.dig("g_value", "date_from"), "%d.%m.%Y"),
+          Date.strptime(document.dig("g_value", "date_to"),   "%d.%m.%Y"),
+          document.dig("g_value", "categories"),
+          document
+      end
+
       def self.pull_from_couchdb uuid
         begin
           response = CouchDB.get_doc uuid
-          return nil if !response["g_value"]["publish_web"]
-          WPEvent::CouchImport::CouchEvent.new response["_id"],
-            response["g_value"]["title"],
-            response["g_value"]["description_long"],
-            Date.strptime(response["g_value"]["date_from"], "%d.%m.%Y"),
-            Date.strptime(response["g_value"]["date_to"],   "%d.%m.%Y"),
-            response["g_value"]["categories"],
-            response
+          return nil if !response.dig("g_value", "publish_web")
+          from_couch_doc response
         rescue Exception => e
+          nil
+        end
+      end
+
+      def self.pull_from_couchdb_between from, to
+        begin
+          response = CouchDB.get_seminar_docs_by_month 2016, 11
+          response.select! {|doc| doc.dig("g_value", "publish_web")}
+          response.map do |document|
+            from_couch_doc document
+          end.compact
+        rescue Exception => e
+          STDERR.puts $! # last exception
+          STDERR.puts $@ # last backtrace
           nil
         end
       end
