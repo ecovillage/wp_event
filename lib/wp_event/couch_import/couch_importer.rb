676ac19cdcb95f30899065cbf18836b6fa38197c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-15 14:41:04 +0100

    CouchImporter#get_all_referees pass-through

--- lib/wp_event/couch_import/couch_importer.rb
+++ lib/wp_event/couch_import/couch_importer.rb
@@ -4,6 +4,10 @@ module WPEvent
       def self.get_all_categories
         CouchDB.get_all_categories.map {|doc| CouchEventCategory.from_couch_doc doc}
       end
+
+      def self.get_all_referees
+        CouchDB.get_all_referees.map {|doc| CouchReferee.from_couch_doc doc}
+      end
     end
   end
 end
