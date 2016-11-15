e9d2f3544c50f0e42bd8e68c04c5f9b756c1a3ec
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-15 02:42:28 +0100

    move logging to cli/logging

--- lib/wp_event.rb
+++ lib/wp_event.rb
@@ -9,7 +9,7 @@ require "wp_event/couch_import/couch_event"
 require "wp_event/couch_import/couch_event_category"
 require "wp_event/couch_import/couch_db"
 require "wp_event/couch_import/couch_importer"
-require "wp_event/logging"
+require "wp_event/cli/logging"
 
 require 'ostruct'
 require 'yaml'
