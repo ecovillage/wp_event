1395d0f0fab31e33cce8dddb22e23b763b6acd41
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-29 23:23:32 +0100

    require post_meta_data from wp_event

--- lib/wp_event.rb
+++ lib/wp_event.rb
@@ -1,6 +1,7 @@
 require "wp_event/version"
 
 require "wp_event/post_type"
+require "wp_event/post_meta_data"
 require "wp_event/category_post"
 require "wp_event/event_post"
 require "wp_event/referee_post"
