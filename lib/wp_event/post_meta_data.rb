ceac4e49b5d6938a19a5edcae5dac480e47a44e3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-11-29 23:23:07 +0100

    add custom_fields / metadata abstraction

---
+++ lib/wp_event/post_meta_data.rb
@@ -0,0 +1,57 @@
+module WPEvent
+  CustomField = Struct.new :id, :key, :value
+
+  class PostMetaData
+    attr_accessor :fields
+
+    def initialize wp_post=nil
+      @fields = (wp_post&.fetch('custom_fields') || []).map do |cf|
+        CustomField.new cf['id'], cf['key'], cf['value']
+      end
+    end
+
+    # Add a custom field
+    def add id, key, value
+      @fields << CustomField.new(id, key, value)
+    end
+
+    # true iff multiple values for key found
+    def multivalued? field_key
+      @fields.count {|f| f.key == field_key} > 1
+    end
+
+    # maps all values with given key
+    def values field_key
+      fields_with_key(field_key).map &:value
+    end
+
+    # returns fields with given key
+    def fields_with_key field_key
+      @fields.select {|f| f.key == field_key}
+    end
+
+    # return FIRST field with given k/v pair
+    def field_with_key_value field_key, field_value
+      @fields.first {|f| f.key == field_key && f.value == field_value}
+    end
+
+    # returns field id for first field with given key
+    def id_for field_key
+      @fields.first {|f| f.key == field_key}&.id
+    end
+
+    def fields_with_key_regex key_regex
+      @fields.select {|f| f.key =~ key_regex}
+    end
+
+    def to_custom_fields_hash
+      result = @fields.map do |field|
+        hsh = {}
+        hsh['key']   = field.key if field.key
+        hsh['value'] = field.value if field.value
+        hsh['id']    = field.id if field.id
+        hsh
+      end
+    end
+  end
+end
