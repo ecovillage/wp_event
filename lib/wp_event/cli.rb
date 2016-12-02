4e55eef6397a87eafc644ebeb8067f0d339fb7fe
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-12-02 07:05:13 +0100

    let tools be more friendly and versatile with pipes

--- lib/wp_event/cli.rb
+++ lib/wp_event/cli.rb
@@ -1,10 +1,22 @@
 module WPEvent
   module CLI
     extend WPEvent::CLI::Logging
+
+    # Logger#error and STDERR.puts given error message
+    # and exits with exit_code
     def errxit exit_code=1, msg
       STDERR.puts msg
       error msg
       exit exit_code
+    end
+
+    # Either opens file with given name or return STDOUT, when '-' is given.
+    def io_out_from_arg outfile
+      if outfile == '-'
+        $stdout
+      else
+        File.open(outfile, 'w')
+      end
     end
   end
 end
