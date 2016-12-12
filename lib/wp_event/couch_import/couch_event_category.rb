34994e4f28a5b51f571aef59abc54c9ac62645ac
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-12-12 07:34:14 +0100

    add CouchEventCategory#image_url

--- lib/wp_event/couch_import/couch_event_category.rb
+++ lib/wp_event/couch_import/couch_event_category.rb
@@ -3,13 +3,15 @@ require 'json'
 module WPEvent
   module CouchImport
     class CouchEventCategory
-      attr_accessor :name, :description, :uuid, :document
-      # shorturl and thumbnail
-      def initialize uuid, name, description, document=nil
+      attr_accessor :name, :description, :uuid, :document, :image_url
+      # shorturl
+
+      def initialize uuid, name, description, document=nil, image_url=nil
         @uuid        = uuid
         @name        = name
         @description = description
         @document    = document
+        @image_url   = image_url
       end
 
       def self.pull_from_couchdb uuid
@@ -24,7 +26,8 @@ module WPEvent
       def to_json *a
         { uuid:        @uuid,
           name:        @name,
-          description: @description
+          description: @description,
+          image_url:   @image_url
         }.to_json(*a)
       end
 
@@ -32,7 +35,8 @@ module WPEvent
         WPEvent::CouchImport::CouchEventCategory.new document["_id"],
           document.dig("g_value", "name"),
           document.dig("g_value", "description"),
-          document
+          document,
+          document.dig("g_value", "thumbnail")
       end
     end
   end
