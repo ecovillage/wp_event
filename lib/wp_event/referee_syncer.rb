d512d018fc67a78ba847c74b6cedfa2ae6ba7588
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-01-15 20:57:36 +0100

    Vastly improve RefereeSyncer before its exodus.
    Let RefereeSyncer do no-op when entities are actually the same.
    This shall be integrated into (the) Syncer before RefereeSyncer is
    made obsolote.

--- lib/wp_event/referee_syncer.rb
+++ lib/wp_event/referee_syncer.rb
@@ -8,16 +8,26 @@ module WPEvent
       @image_uploader = image_uploader
     end
 
+    # wp_referee is id, comes from wp.getPost, json is newer version
     def create_or_update wp_referee, referee_json
       attachment_id = @image_uploader.process referee_json['image_url']
 
       if wp_referee
-        info "Referee with UUID #{referee_json['uuid']} found, updating"
-        WPEvent::RefereePost.update wp_referee,
-                                    referee_json["firstname"],
-                                    referee_json["lastname"],
-                                    referee_json["description"],
-                                    attachment_id
+        same =  WPEvent::RefereePost.same?(wp_referee,
+                                           referee_json["firstname"],
+                                           referee_json["lastname"],
+                                           referee_json["description"],
+                                           attachment_id)
+        if same
+          info "Identical values, no need to update"
+        else
+          info "Referee with UUID #{referee_json['uuid']} found, updating"
+          WPEvent::RefereePost.update wp_referee,
+                                      referee_json["firstname"],
+                                      referee_json["lastname"],
+                                      referee_json["description"],
+                                      attachment_id
+        end
       else
         info "Referee with UUID #{referee_json['uuid']} not found, creating"
         wp_referee = WPEvent::RefereePost.create referee_json["uuid"],
