7824be456f2a44589797d0771fb8fcce3c057219
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-01-16 17:51:20 +0100

    implement Eventtest/event_cpt_test.rbadd_referee

--- lib/wp_event/custom_post_types/event.rb
+++ lib/wp_event/custom_post_types/event.rb
@@ -1,6 +1,8 @@
 module WPEvent
   module CustomPostTypes
     class Event < CustomPostType
+      attr_accessor :referee_id_qualification_map
+
       wp_post_type 'ev7l-event'
       wp_post_title_alias    'name'
       wp_post_content_alias  'description'
@@ -9,6 +11,17 @@ module WPEvent
       wp_custom_field_single 'todate'
       wp_custom_field_multi  'event_category_id'
       wp_custom_field_multi  'referee_id'
+
+      def initialize(**kwargs)
+        super(**kwargs)
+        @referee_id_qualification_map = {}
+      end
+
+      def add_referee(id, qualification)
+        @referee_id_qualification_map[id] = qualification
+        ref_qa_field_name = "referee_#{id}_qualification"
+        @fields[ref_qa_field_name] = CustomFieldValue.new(nil, ref_qa_field_name, qualification)
+      end
     end
   end
 end
