bede97db7f1ca4aaeecb1ecc43127240d47b4d08
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-01-17 21:42:11 +0100

    add more fields to Event

--- lib/wp_event/custom_post_types/event.rb
+++ lib/wp_event/custom_post_types/event.rb
@@ -4,13 +4,24 @@ module WPEvent
       attr_accessor :referee_id_qualification_map
 
       wp_post_type 'ev7l-event'
+
       wp_post_title_alias    'name'
       wp_post_content_alias  'description'
+
+      wp_custom_field_multi  'event_category_id'
+      wp_custom_field_multi  'referee_id'
+
       wp_custom_field_single 'uuid'
       wp_custom_field_single 'fromdate' # Ideally: type: :date, out: :to_i
       wp_custom_field_single 'todate'
-      wp_custom_field_multi  'event_category_id'
-      wp_custom_field_multi  'referee_id'
+      wp_custom_field_single 'arrival'
+      wp_custom_field_single 'departure'
+      wp_custom_field_single 'current_infos'
+      wp_custom_field_single 'costs_participation'
+      wp_custom_field_single 'costs_catering'
+      wp_custom_field_single 'info_housing'
+      wp_custom_field_single 'participants_please_bring'
+      wp_custom_field_single 'participants_prerequisites'
 
       def initialize(**kwargs)
         super(**kwargs)
